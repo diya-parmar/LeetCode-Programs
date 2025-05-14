5af994d5f48606f6b43de0fe4f58cd70b358720b
Diya Parmar <diyanparmar18@gmail.com>
2025-05-14 22:32:47 +0530

    Solved Divide Two Integers.java

---
+++ Divide Two Integers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if(dividend == Integer.MIN_VALUE && divisor == -1){
+            return Integer.MAX_VALUE;
+        }
+        boolean negative = (dividend < 0) ^ (divisor < 0);
+        
+        long absDividend = Math.abs((long) dividend);
+        long absDivisor = Math.abs((long) divisor);
+        int Quotient = 0;
+
+        while(absDividend >= absDivisor){
+            long temp = absDivisor;
+            long multiple = 1;
+
+            while(absDividend >= (temp << 1)){
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            absDividend -= temp;
+            Quotient += multiple;
+        }
+        return negative ? -Quotient : Quotient;
+    }
+}
