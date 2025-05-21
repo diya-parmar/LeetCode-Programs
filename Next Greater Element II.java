2f8a747bf64feff182423b3693a5eef883d4daf9
Diya Parmar <diyanparmar18@gmail.com>
2025-05-21 09:48:41 +0530

    Solved Next Greater Element II.java

---
+++ Next Greater Element II.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < 2 * n; i++){
+            while(!stack.isEmpty() && nums[stack.peek()] < nums[i % n]){
+                result[stack.pop()] = nums[i % n];
+            }
+            if(i < n){
+                stack.push(i);
+            }
+        }
+        while(!stack.isEmpty()){
+            result[stack.pop()] = -1;
+        }
+        return result;
+    }
+}
